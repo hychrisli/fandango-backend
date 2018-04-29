fbe7c07b95dfb30a4940daf3e6dfd347d750d876
Chris Li <hychrisli@users.noreply.github.com>
2018-04-28 18:27:06 -0700

    Filter schedule results

---
+++ src/main/java/cmpe273/fandango/dto/ParamFilterSchedule.java
@@ -0,0 +1,13 @@
+package cmpe273.fandango.dto;
+
+public class ParamFilterSchedule {
+
+  private Float minPrice;
+
+  private Float maxPrice;
+
+  private Integer minStars;
+
+  private Integer maxStars;
+
+}
